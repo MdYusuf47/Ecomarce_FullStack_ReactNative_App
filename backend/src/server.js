c9255e4864dcd4dd72c2c0f8b54f3af586b703d2
Md Yusuf <mdyusuf742171@gmail.com>
2026-01-18 22:13:05 +0530

    db config setup

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,23 +1,28 @@
-import express from 'express';
-import path from 'path';
-import { ENV } from './config/env.js';
+import express from "express";
+import path from "path";
+import { ENV } from "./config/env.js";
+import { connectDB } from "./config/db.js";
+import { clerkMiddleware } from '@clerk/express'
 
 const app = express();
 
 const __dirname = path.resolve();
 
+app.use(clerkMiddleware())
 
-app.get("/api/health",(req,res) => {
-    res.status(200).json({message:"Success"})
-})
+app.get("/api/health", (req, res) => {
+  res.status(200).json({ message: "Success" });
+});
 
-if (ENV.NODE_ENV === 'production') {
-    app.use(express.static(path.join(__dirname,'../admin/dist')));
+if (ENV.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "../admin/dist")));
 
-    app.get("/{*any}",(req,res) => {
-        res.sendFile(path.join(__dirname,"../admin","dist","index.html"));
-    })
+  app.get("/{*any}", (req, res) => {
+    res.sendFile(path.join(__dirname, "../admin", "dist", "index.html"));
+  });
 }
 
-
-app.listen(ENV.PORT,() => console.log("Server running on port 3000"));
+app.listen(ENV.PORT, () => {
+  console.log("Server is up and running");
+  connectDB();
+});
